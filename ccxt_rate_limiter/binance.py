704059f7414d11b2d5b28e43b4cf3122b700287e
a <a@example.com>
2021-10-17 22:00:08 +0900

    update binance defs

--- ccxt_rate_limiter/binance.py
+++ ccxt_rate_limiter/binance.py
@@ -8,6 +8,18 @@ def binance_wrap_defs():
             'count': 1,
         },
         {
+            # https://binance-docs.github.io/apidocs/futures/en/#kline-candlestick-data
+            'regex': 'Get.*Klines',
+            'tags': ['all'],
+            'count': 10 - 1,
+        },
+        {
+            # https://binance-docs.github.io/apidocs/futures/en/#account-information-v2-user_data
+            'regex': 'Get.*Account',
+            'tags': ['all'],
+            'count': 5 - 1,
+        },
+        {
             'regex': 'Post.*Order',
             'tags': ['send_order'],
             'count': 1,
